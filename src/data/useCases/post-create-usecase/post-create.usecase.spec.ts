4b7bdf0ba1f26483341f00077b2b7211d54e2f2b
junioralcant <junioralcant111@gmail.com>
2023-11-11 10:35:15 -0300

    test: ensure PostCreateUseCase return method create with correct data

--- src/data/useCases/post-create-usecase/post-create.usecase.spec.ts
+++ src/data/useCases/post-create-usecase/post-create.usecase.spec.ts
@@ -3,6 +3,7 @@ import {describe, expect, it} from 'vitest';
 import {AxiosHttpClientInMemory} from '../../../infra/implementations/axios-http-client/in-memory/axios-http-client.in.memory';
 import {postsListAPIMock} from '../../mocks/post-list.mock';
 import {PostCreateUseCase} from './post-create.usecase';
+import {PostModel} from '../../../domain/models/post.model';
 
 type SutParam = {
   axiosHttpClientInMemory?: AxiosHttpClientInMemory;
@@ -18,7 +19,6 @@ function makeSut({
 describe('PostCreateUseCase', () => {
   it('Should return method list with correct method and URL', async () => {
     const axiosHttpClientInMemory = new AxiosHttpClientInMemory();
-    axiosHttpClientInMemory.response = {data: postsListAPIMock()};
 
     const {sut} = makeSut({axiosHttpClientInMemory});
     await sut.create({title: 'meu titulo', body: 'meu body'});
@@ -31,5 +31,30 @@ describe('PostCreateUseCase', () => {
       title: 'meu titulo',
       body: 'meu body',
     });
+  });
+
+  it('Should return method create with correct data', async () => {
+    const axiosHttpClientInMemory = new AxiosHttpClientInMemory();
+    const postsListAPI = {
+      id: '04',
+      title_post: 'Meu titulo',
+      body_post: 'Meu body',
+    };
+
+    axiosHttpClientInMemory.response = {
+      data: postsListAPI,
+    };
+
+    const {sut} = makeSut({axiosHttpClientInMemory});
+    const data = await sut.create({
+      title: 'Meu titulo',
+      body: 'Meu body',
+    });
+
+    expect(data).toEqual({
+      id: postsListAPI.id,
+      title: postsListAPI.title_post,
+      body: postsListAPI.body_post,
+    } as PostModel);
   });
 });
