36b0024f3d251dc4ca1ae5bcad5ed16293ce1ff2
junioralcant <junioralcant111@gmail.com>
2023-11-25 10:12:45 -0300

    chore: add role in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,5 +23,6 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
   },
 };
