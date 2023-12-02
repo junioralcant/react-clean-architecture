4dfbe44c81f74c3485459d9e51d6ca01f4adf781
junioralcant <junioralcant111@gmail.com>
2023-12-02 14:49:42 -0300

    chore: config abisolute path in application

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 import path from 'path';
 
@@ -11,7 +12,7 @@ const SETUP_TESTS_DIR = path.resolve(
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     test: {
       setupFiles: [SETUP_TESTS_DIR],
       environment: 'jsdom',
